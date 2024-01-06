b4b21ace494ce2a283b83870e2a3dd9d78b04c0b
AnjasFedo <fedoaf98@gmail.com>
2024-01-06 12:34:33 +0700

    Footer Component

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -26,7 +26,6 @@
     ];
 </script>
 
-lorem
 <main class="flex flex-col flex-1 p-4">
     <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
@@ -41,7 +40,7 @@ lorem
             <a
                 href="#"
                 target="_blank"
-                class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
             >
                 <div
                     class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
