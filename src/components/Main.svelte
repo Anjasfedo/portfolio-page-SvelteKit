5ff8292ba2b9c7d0fbd802d5f3ca2857c31077e5
AnjasFedo <fedoaf98@gmail.com>
2024-01-06 11:23:10 +0700

    Header & Main Component

---
+++ src/components/Main.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Anjas</span>Fedo <br />Full Stack
+                <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">Favorite Tech</span> includes JavaScript (Next.JS or SvelteKit), TailwindCSS,
+                Node.JS + Express.JS & MongoDB or Firebase/Firestore
+            </p>
+            <a
+                href="#"
+                target="_blank"
+                class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+            >
+                <div
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                ></div>
+                <h4 class="relative z-10">Get in touch &rarr;</h4></a
+            >
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"images/img-blank.PNG"} alt="Profile Image" class="object-cover z-[2] max-h-[70vh]" />
+        </div>
+    </section>
+</main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,31 @@
 <script>
     import "../app.css";
+    import Header from "../components/Header.svelte";
+    import Footer from "../components/Footer.svelte";
+
+    let y;
+    let innerHeight = 0;
+    let innerWidth = 0;
+
+    const goTop = () => {
+        document.body.scrollIntoView();
+    };
 </script>
 
-<slot />
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+    <div
+        class={"fixed bottom-0 w-full duration-200 flex p-10 z=[10] " +
+            (y > 0 ? " opacity-full pointer-events-auto" : " opacity-0 pointer-events-none")}
+    >
+        <button
+            on:click={goTop}
+            class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+            ><i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
+        </button>
+    </div>
+    <Header {y} />
+    <slot />
+    <Footer />
+</div>
+
+<svelte:window bind:scrollX={y} bind:innerHeight bind:innerWidth />
