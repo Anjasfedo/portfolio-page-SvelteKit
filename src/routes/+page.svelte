50d91582937e33311d6db85e60e73c3881e909ef
AnjasFedo <fedoaf98@gmail.com>
2024-01-06 10:39:23 +0700

    ParticlesJS initialize

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,2 @@
-<h1 class="text-3xl font-bold underline uppercase">
-    Hello world!
-  </h1>
-  
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
